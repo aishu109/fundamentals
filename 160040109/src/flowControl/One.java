e5f2cd6fd034fc940674b545b91a3d136577ca54
Dhathri Aishwarya <aishusriha@gmail.com>
2019-06-25 16:21:54 +0530

    2nd  commit

---
+++ 160040109/src/flowControl/One.java
@@ -0,0 +1,18 @@
+package flowControl;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int i=Integer.parseInt(args[0]);
+			if(i%2==0)
+			System.out.println("even number");
+			else
+			System.out.println("odd number");
+			}
+}
+
+
+	
+
+
---
+++ 160040109/src/flowControl/Two.java
@@ -0,0 +1,17 @@
+package flowControl;
+
+public class Two {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+int i=Integer.parseInt(args[0]);
+if(i==0)
+System.out.println("zero");
+else if(i>0)
+System.out.println("positive");
+else
+System.out.println("negative");
+}
+}
